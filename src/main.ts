2e972553a5c3306fdc155c4085f6747432dccb3d
Usman-Ahmad673 <itsusmanahmad00@gmail.com>
2025-01-08 15:34:52 +0500

    added ui for home route

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './users/all-exceptions.filter';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // const app = await NestFactory.create(AppModule, {
   //   bufferLogs: true
   // });
   // app.useLogger(app.get(LoggerService))
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  // Set the views directory and engine
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.enableCors();
